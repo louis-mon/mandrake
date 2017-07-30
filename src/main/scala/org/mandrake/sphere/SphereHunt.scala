8bbea2245f184faa423a36c0b78f49b0d4c7ac92
louis <louis.monestier@hotmail.fr>
2017-07-30 17:36:04 +0200

    display players

--- src/main/scala/org/mandrake/sphere/SphereHunt.scala
+++ src/main/scala/org/mandrake/sphere/SphereHunt.scala
@@ -3,20 +3,42 @@ package org.mandrake.sphere
 import org.mandrake.runners.slick2d.Slick2DRunner
 import org.mandrake.runners.slick2d.Slick2DRunner.{RenderEvent, SlickInput}
 import org.mandrake.simulation.{SimpleInputState, Simulation, StateOutput}
-import org.newdawn.slick.Graphics
+import org.newdawn.slick.{Color, Graphics}
 import org.newdawn.slick.geom.Circle
 
-class SphereHunt extends SimpleInputState[SlickInput] {
+case class Player(angle: Float, color: Color)
+
+class SphereHuntInit() extends SimpleInputState[SlickInput] {
   override def apply(event: SlickInput): StateOutput = {
-    val sphereRadius = Math.min(event.container.getHeight, event.container.getWidth) / 3
-    StateOutput.apply(this, new RenderEvent {
+    val input = event.container.getInput
+    val nbPlayers = Math.min(input.getControllerCount, 4)
+    val colors = Vector(Color.blue, Color.red, Color.green, Color.yellow, Color.cyan, Color.magenta)
+    StateOutput(SphereHunt(0.until(nbPlayers).map(i => Player(i*2 * Math.PI.toFloat / nbPlayers, colors(i))).toVector))
+  }
+}
+
+case class SphereHunt(players: Vector[Player]) extends SimpleInputState[SlickInput] {
+  override def apply(event: SlickInput): StateOutput = {
+    val container = event.container
+    val sphereRadius = Math.min(container.getHeight, container.getWidth) / 3
+    val playerSize = 20
+    StateOutput(this, new RenderEvent {
       override def render(g: Graphics): Unit = {
-        g.draw(new Circle(event.container.getWidth / 2, event.container.getHeight / 2, sphereRadius))
+        g.pushTransform()
+        g.translate(container.getWidth / 2, container.getHeight / 2)
+        g.setColor(Color.white)
+        g.draw(new Circle(0, 0, sphereRadius))
+        players.foreach{player =>
+          g.setColor(player.color)
+          g.fill(new Circle((sphereRadius + playerSize) * Math.cos(player.angle).toFloat,
+            (sphereRadius + playerSize) * Math.sin(player.angle).toFloat, playerSize))
+        }
+        g.popTransform()
       }
     })
   }
 }
 
 object SphereHunt extends App {
-  Slick2DRunner.run(Simulation(Vector(new SphereHunt())))
+  Slick2DRunner.run(Simulation(Vector(new SphereHuntInit())))
 }
